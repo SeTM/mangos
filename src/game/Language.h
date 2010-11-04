deb9744c993dd9ade8f965ce9a7df998d9cdd764
SeT <set@wowacadem.ru>
2010-11-05 01:18:47 +0600

    Revert "Added new version of [patch 123]"
    Conflicts:
    
    	src/game/Language.h

--- src/game/Language.h
+++ src/game/Language.h
@@ -903,7 +903,6 @@ enum MangosStrings
 
     // Use for custom patches             11000-11999
     LANG_NAMEANNOUNCE                   = 11001,
-    LANG_ANNOUNCE_CHEAT                 = 11000,
 
     // NOT RESERVED IDS                   12000-1999999999
     // `db_script_string` table index     2000000000-2000009999 (MIN_DB_SCRIPT_STRING_ID-MAX_DB_SCRIPT_STRING_ID)
