499b2d12108dc344979f57900156d548854ab120
SeT <set@wowacadem.ru>
2010-11-18 03:09:25 +0600

    Update scripts

--- src/bindings/ScriptDev2/scripts/kalimdor/caverns_of_time/dark_portal/dark_portal.cpp
+++ src/bindings/ScriptDev2/scripts/kalimdor/caverns_of_time/dark_portal/dark_portal.cpp
@@ -289,16 +289,12 @@ struct MANGOS_DLL_DECL npc_time_riftAI : public ScriptedAI
             return;
         }
 
-        float x,y,z;
-        m_creature->GetRandomPoint(m_creature->GetPositionX(),m_creature->GetPositionY(),m_creature->GetPositionZ(),10.0f,x,y,z);
+        float x, y, z;
+        m_creature->GetRandomPoint(m_creature->GetPositionX(), m_creature->GetPositionY(), m_creature->GetPositionZ(), 10.0f, x, y, z);
+        // uncomment the following if something doesn't work correctly, otherwise just delete
+        // m_creature->UpdateAllowedPositionZ(x, y, z);
 
-        //normalize Z-level if we can, if rift is not at ground level.
-        z = std::max(m_creature->GetMap()->GetHeight(x, y, MAX_HEIGHT), m_creature->GetMap()->GetWaterLevel(x, y, z));
-
-        Unit *Summon = m_creature->SummonCreature(creature_entry,x,y,z,m_creature->GetOrientation(),
-            TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT,30000);
-
-        if (Summon)
+        if (Unit *Summon = m_creature->SummonCreature(creature_entry, x, y, z, m_creature->GetOrientation(), TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000))
         {
             if (Creature *temp = m_creature->GetMap()->GetCreature(m_pInstance->GetData64(DATA_MEDIVH)))
                 Summon->AddThreat(temp);
--- src/bindings/ScriptDev2/scripts/outland/hellfire_peninsula.cpp
+++ src/bindings/ScriptDev2/scripts/outland/hellfire_peninsula.cpp
@@ -254,19 +254,17 @@ struct MANGOS_DLL_DECL npc_demoniac_scryerAI : public ScriptedAI
 
         switch(m_uiButtressCount)
         {
-            case 1: fAngle = 0.0f; break;
-            case 2: fAngle = M_PI_F+M_PI_F/2; break;
-            case 3: fAngle = M_PI_F/2; break;
-            case 4: fAngle = M_PI_F; break;
+        case 1: fAngle = 0.0f; break;
+        case 2: fAngle = M_PI_F+M_PI_F/2; break;
+        case 3: fAngle = M_PI_F/2; break;
+        case 4: fAngle = M_PI_F; break;
         }
 
-        float fX, fY;
-        m_creature->GetNearPoint2D(fX, fY, 5.0f, fAngle);
-
-        float fZ_Ground = m_creature->GetMap()->GetHeight(fX, fY, MAX_HEIGHT);
+        float fX, fY, fZ;
+        m_creature->GetNearPoint(m_creature, fX, fY, fZ, 0.0f, 5.0f, fAngle);
 
         uint32 uiTime = TIME_TOTAL - (m_uiSpawnButtressTimer * m_uiButtressCount);
-        m_creature->SummonCreature(NPC_BUTTRESS, fX, fY, fZ_Ground, m_creature->GetAngle(fX, fY), TEMPSUMMON_TIMED_DESPAWN, uiTime);
+        m_creature->SummonCreature(NPC_BUTTRESS, fX, fY, fZ, m_creature->GetAngle(fX, fY), TEMPSUMMON_TIMED_DESPAWN, uiTime);
     }
 
     void DoSpawnDemon()
