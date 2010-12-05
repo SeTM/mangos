fcf267ec3ea974fc79d442fa4170bd69de44caca
SeT <set@wowacadem.ru>
2010-12-06 00:32:56 +0600

    comment

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -4270,7 +4270,7 @@ bool Unit::AddSpellAuraHolder(SpellAuraHolder *holder)
                 case SPELL_AURA_PERIODIC_ENERGIZE:      // all or self or clear non-stackable
                 default:                                // not allow
                     // can be only single (this check done at _each_ aura add
-                    RemoveSpellAuraHolder(foundHolder,SpellEffectIndex(i),AURA_REMOVE_BY_STACK);
+                    RemoveSpellAuraHolder(foundHolder,/*SpellEffectIndex(i),*/AURA_REMOVE_BY_STACK);
                     stop = true;
                     break;
                 }
