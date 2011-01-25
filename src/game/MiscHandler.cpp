b8ddfcb745329f959f365ff7f860c9720ec0ea2a
SeT <set@wowacadem.ru>
2011-01-25 15:49:28 +0600

    confirmation dialog for players upon entering instance which would permanently bind the player to it, players can now choose to get saved or leave the instance.
    Conflicts:
    
    	src/game/BattleGroundDS.h
    	src/game/MiscHandler.cpp
    	src/game/Player.cpp

--- src/game/MiscHandler.cpp
+++ src/game/MiscHandler.cpp
@@ -1607,3 +1607,23 @@ void WorldSession::HandleAcceptGrantLevel(WorldPacket& recv_data)
 
     _player->GiveLevel(_player->getLevel() + 1);
 }
+
+void WorldSession::HandleInstanceLockResponse(WorldPacket& recvPacket)
+{
+    printf("\n ! got HandleInstanceLockResponse ! \n ");
+    uint8 accept;
+    recvPacket >> accept;
+
+    if (!_player->HasPendingBind())
+    {
+        sLog.outDetail("InstanceLockResponse: Player %s (guid %u) tried to bind himself/teleport to graveyard without a pending bind!", _player->GetName(), _player->GetGUIDLow());
+        return;
+    }
+
+    if (accept)
+        _player->BindToInstance();
+    else
+        _player->RepopAtGraveyard();
+
+    _player->SetPendingBind(NULL, 0);
+}
