a95732510b574c84a9469941de23c14a72c0e4f0
SeT <set@wowacadem.ru>
2011-02-09 06:30:50 +0600

    Update scripts

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "f5886eab425181a433dec882fa1875baf2b29bc0"
+ #define REVISION_ID "8d2588954e4d83259856ce4cbc45ea967dbfb17a"
  #define REVISION_DATE "*"
  #define REVISION_TIME "*"
 #endif // __REVISION_H__
