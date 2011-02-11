1568316ed99ef109d5a742050d34fad5b2657fb8
SeT <set@wowacadem.ru>
2011-02-11 23:19:34 +0600

    merge sd

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "88ac54cc0d19416d71155e63f863b9a70b1a50d4"
+ #define REVISION_ID "*"
  #define REVISION_DATE "*"
  #define REVISION_TIME "*"
 #endif // __REVISION_H__
