30f60e0b92af9d7a67fd24abc12d25dfca147997
SeT <set@wowacadem.ru>
2011-03-18 15:55:06 +0600

    Merge branch 'master' of git://github.com/rsa/scriptdev2
    Conflicts:
    	scripts/northrend/icecrown_citadel/icecrown_citadel/boss_professor_putricide.cpp
    	scripts/northrend/icecrown_citadel/icecrown_citadel/boss_sindragosa.cpp
    	scripts/northrend/icecrown_citadel/icecrown_citadel/def_spire.h
    	scripts/northrend/icecrown_citadel/icecrown_citadel/icecrown_citadel.h
    	scripts/northrend/icecrown_citadel/icecrown_citadel/instance_icecrown_citadel.cpp
    	scripts/northrend/icecrown_citadel/icecrown_citadel/instance_icecrown_spire.cpp

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "4f266fe8e1870c3385b12c70264b2b4a76917c9d"
+ #define REVISION_ID "35a7536bc0fbedb9940f8ea92df0d8a121ab462d"
  #define REVISION_DATE "*"
  #define REVISION_TIME "*"
 #endif // __REVISION_H__
