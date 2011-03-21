113345cde30558432f8eb02ba24a968bfa1028fd
SeT <set@wowacadem.ru>
2011-03-22 00:33:00 +0600

    upd rev

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "38d8496fa482a848a78e518141a5e95f4a568fbc"
+ #define REVISION_ID "eed2cf1e57722648dd4c1c32b2569aa23ef70bea"
  #define REVISION_DATE "*"
  #define REVISION_TIME "*"
 #endif // __REVISION_H__
